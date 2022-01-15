6a3375ce23f5bac85d03d8eeb2d8b421f41db83b
Muhammad Ahmad <181370123@gift.edu.pk>
2022-01-15 23:12:50 +0500

    setup CRUD routes for notes

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(routes)
 
 app.use((req,res) => {
--- routes/notes.js
+++ routes/notes.js
@@ -1,19 +1,29 @@
 export const list = (req, res) => {
+  const { sort } = req.query;
+  console.log({ sort });
   res.json([])
 }
 
 export const create = (req, res) => {
+  const { title, body } = req.body
+  console.log(`${title} and ${body} received`);
   res.send('ok')
 }
 
 export const read = (req, res) => {
-  res.json({ title: 'note title', body: 'this is my note' })
+  const { id } = req.params
+  res.json({ id, title: 'note title', body: 'this is my note' })
 }
 
 export const update = (req, res) => {
+  const { id } = req.params
+  const { title, body } = req.body
+  console.log(`updating ${id} with ${title} and ${body}`);
   res.send('ok')
 }
 
 export const deleteNote = (req, res) => {
+  const { id } = req.params
+  console.log(`${id} note deleted`)
   res.send('ok')
 }
