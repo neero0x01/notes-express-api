a30792b2c082b1cfcc293b9de0e776b7119db2e4
Muhammad Ahmad <181370123@gift.edu.pk>
2022-01-15 22:36:09 +0500

    Add error handling

--- index.js
+++ index.js
@@ -10,8 +10,18 @@ app.get('/', (req, res) => {
   res.send('hola')
 })
 
+app.get('/error', (req, res) => {
+  throw new Error("oops I made a mistake")
+})
+
 app.use((req,res) => {
   res.status(404).send("Not found");
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send(err.message)
+  next()
 })
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
