ed77ac943e4b64ea276508af7fb98bdd83d1beef
Muhammad Ahmad <181370123@gift.edu.pk>
2022-01-15 22:26:12 +0500

    setup 404 message

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from 'express'
+import morgan from 'morgan';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(morgan("dev"));
+
 app.get('/', (req, res) => {
   res.send('hola')
+})
+
+app.use((req,res) => {
+  res.status(404).send("Not found");
 })
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
