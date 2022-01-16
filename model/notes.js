19c8b44ef46b747a70827b465df8f19112a4dc9f
Muhammad Ahmad <181370123@gift.edu.pk>
2022-01-16 23:59:52 +0500

    add note model

---
+++ model/notes.js
@@ -0,0 +1,44 @@
+import { v4 as uuid } from 'uuid';
+const NOTES = new Map()
+
+// Note {
+//   id: string
+//   title: string
+//   body: string
+//   lastEdited: Date
+// }
+
+export const getNotes = () => {
+  return Array.from(NOTES.values())
+}
+
+export const createNote = ({ title, body }) => {
+  const id = uuid()
+  const lastEdited = Date.now()
+  const note = {
+    id,
+    lastEdited,
+    title,
+    body
+  }
+  NOTES.set(id, note)
+  return { ...note };
+}
+
+export const updateNote = ({ title, body }) => {
+  if(!NOTES.has(id)) return null
+  const note = NOTES.get(id)
+  note.title = title ?? note.title
+  note.body = body ?? note.body
+  return { ...note }
+}
+
+export const getNote = (id) => {
+  if(!NOTES.has(id)) return null
+  const note = NOTES.get(id)
+  return { ...note }
+}
+
+export const delteNote = (id) => {
+  return NOTES.delete(id)
+}
